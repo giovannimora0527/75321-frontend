96ff4a85cff57c7d8ce837d5fd79484425b99c5c
samuel <samusplay08@gmail.com>
2025-09-29 20:13:49 -0500

    servicio consumido

---
+++ src/app/demo/pages/usuario/service/usuario.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from 'src/app/services/backend.service';
+import { environment } from 'src/environments/environment';
+import { Usuario } from '../model/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  //atributos endpoint y api url
+  private apiUrl = environment.apiUrlAuth;
+  private endpoint = 'usuarios';
+
+  constructor(private readonly backend: BackendService) {}
+
+  //Definimos el metodo para luego inyectarlo al componente
+  //observable es una promesa mejorada
+
+  buscarporDocumento(numDoc:string):Observable<Usuario>{
+    //Construimos la url
+    return this.backend.get(this.apiUrl,this.endpoint,`buscar-por-documento/${numDoc}`)
+  }
+}
--- src/app/demo/pages/usuario/usuario.component.ts
+++ src/app/demo/pages/usuario/usuario.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Usuario } from './model/usuario';
+import { UsuarioService } from './service/usuario.service';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.scss'
 })
 export class UsuarioComponent {
+  usuariosList: Usuario[] = [];
 
+  constructor(private readonly usuarioService: UsuarioService) {
+    this.listarUsuarios();
+  }
+
+  otroMetodo() {
+    console.log('Usuarios cargados: ', this.usuariosList);
+  }
+
+  //Tiene que existir el metodo en el servicio para luego implentar logica de Negocio
+  listarUsuarios() {
+    console.log('Entro a cargar usuarios');
+    this.usuarioService.buscarporDocumento('9902001008').subscribe({
+      next:Usuario=>{
+        this.usuariosList=[Usuario];
+        this.otroMetodo
+      },
+      error: err=>console.error('Error al cargar usuarios',err)
+    });
+  }
+  
 }
